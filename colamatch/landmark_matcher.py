f570d2de55c091796f8ae065a709cc5464d54d48
Sarah <sa.haas@fz-juelich.de>
2018-03-02 16:23:58 +0100

    changed parameter names; added docstrings

--- colamatch/landmark_matcher.py
+++ colamatch/landmark_matcher.py
@@ -16,11 +16,18 @@ class Matcher(ABC):
         """ Abstract matching method. """
         pass
 
-    def get_candidates(self, l_fixed, l_moving):
-        """ Get landmarks with distance < max_matching_distance. """
+    def get_candidates(self, L_fixed, L_moving):
+        """ Get landmarks with distance < max_matching_distance.
+
+        Args:
+            L_fixed (array-like): Landmarks in fixed image (x,y coordinates).
+            L_moving (array-like): Landmarks in moving image (x,y coordinates).
+        Returns:
+            Array with index pairs of L_fixed and L_moving defining match candidates (shape (N,2)).
+        """
         possibleMatches = np.zeros((0,3))
-        for i,k0 in enumerate(l_fixed):
-            for j,k1 in enumerate(l_moving):
+        for i,k0 in enumerate(L_fixed):
+            for j,k1 in enumerate(L_moving):
                 value = 1-(np.sqrt(((np.array(k0[:2])-np.array(k1[:2]))**2).sum()) / float(self.max_matching_distance))
                 if value >= 0:
                     possibleMatches = np.append(possibleMatches, [[i,j,value]], axis=0)
@@ -31,6 +38,7 @@ class Matcher(ABC):
         """ Perform homography ransac. """
         pass
 
+
 class TemplateMatcher(Matcher):
     """ TemplateMatcher class. """
 
@@ -40,13 +48,23 @@ class TemplateMatcher(Matcher):
         self.patchsize = patchsize
         self.cv2_matching_method = cv2_matching_method
 
-    def match(self, l_fixed, l_moving, img_fixed, img_moving, candidates=None, num_best_matches=1):
-        """ Perform template matching. """
+    def match(self, L_fixed, L_moving, img_fixed, img_moving, candidates=None, num_best_matches=1):
+        """ Perform template matching.
+        Args:
+            L_fixed (array-like): Landmarks in fixed image (x,y coordinates).
+            L_moving (array-like): Landmarks in moving image (x,y coordinates).
+            img_fixed (array-like): Fixed image.
+            img_moving (array-like): Moving image.
+            candidates (array-like, optional): Indices of L_fixed and L_moving defining match candidates.
+            num_best_matches (int, optional): Number of best matches returned per landmark.
+        Returns:
+            Array of matched landmark index pairs of L_fixed and L_moving (shape (N,2)).
+        """
         if candidates is None:  # every match is possible
-            candidates = np.array(np.where(np.ones((len(l_fixed),len(l_moving))))).T
+            candidates = np.array(np.where(np.ones((len(L_fixed),len(L_moving))))).T
         scores = []
         for candidate in candidates:
-            scores.append(self._template_matching(l_fixed[candidate[0]], l_moving[candidate[1]], img_fixed, img_moving))
+            scores.append(self._template_matching(L_fixed[candidate[0]], L_moving[candidate[1]], img_fixed, img_moving))
         scores = np.array(scores)
         # for squared differences, small scores are best -> sorted scores must be reversed later
         sort_scores = -1 if self.cv2_matching_method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED] else 1
@@ -86,27 +104,22 @@ class TriangleMatcher(Matcher):
     def _triangle_ransac(self, candidates):
         pass
 
-    def _is_mirrored(self, ABC, DEF, l_fixed, l_moving):
+    def _is_mirrored(self, ABC, DEF, L_fixed, L_moving):
         """ Check if Triangles ABC and DEF are mirrored.
 
         Args:
             ABC (array_like): Three indices of l_fixed defining a triangle.
             DEF (array_like): Three indices of l_moving defining a triangle.
-            l_fixed (array_like): Array of all landmarks of fixed image with at least columns x and y.
-            l_moving (array_like): Array of all landmarks of moving image with at least columns x and y.
+            L_fixed (array_like): Array of all landmarks of fixed image with at least columns x and y.
+            L_moving (array_like): Array of all landmarks of moving image with at least columns x and y.
         Returns:
             True if ABC and DEF are mirrored, else False.
         """
-        ABC_Coords = np.array([l_moving[ABC[0]][:2], l_moving[ABC[1]][:2], l_moving[ABC[2]][:2], l_moving[ABC[0]][:2]])
-        DEF_Coords = np.array([l_fixed[DEF[0]][:2], l_fixed[DEF[1]][:2], l_fixed[DEF[2]][:2], l_fixed[DEF[0]][:2]])
+        ABC_Coords = np.array([L_moving[ABC[0]][:2], L_moving[ABC[1]][:2], L_moving[ABC[2]][:2], L_moving[ABC[0]][:2]])
+        DEF_Coords = np.array([L_fixed[DEF[0]][:2], L_fixed[DEF[1]][:2], L_fixed[DEF[2]][:2], L_fixed[DEF[0]][:2]])
         sumABC = 0
         sumDEF = 0
         for i in range(1, 4):
             sumABC += (ABC_Coords[i][1] + ABC_Coords[i-1][1]) * (ABC_Coords[i][0] - ABC_Coords[i-1][0])
             sumDEF += (DEF_Coords[i][1] + DEF_Coords[i-1][1]) * (DEF_Coords[i][0] - DEF_Coords[i-1][0])
         return np.sign(sumABC) != np.sign(sumDEF)
-
-
-if __name__ == "__main__":
-    tm = TemplateMatcher(5, 7)
-    tm.match(1,2,3,4,5)
